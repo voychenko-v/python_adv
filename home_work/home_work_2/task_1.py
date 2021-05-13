a5297f20e0da501d50cf086be785340fd7604985
voychenko-v <voychenkovitaliy@gmail.com>
2021-05-13 23:22:12 +0300

    hw_1 - t1, t2

--- home_work/home_work_2/task_1.py
+++ home_work/home_work_2/task_1.py
@@ -1,14 +1,13 @@
 class Car:
-    is_police = False
 
-    def __init__(self, speed, color, name):
+    def __init__(self, speed, color, name, is_police=False):
         self.speed = speed
         self.color = color
         self.name = name
-        # self.is_police = is_police
+        self.is_police = is_police
 
     def show_speed(self):
-        return print(f'Скорость {self.speed} км/ч')
+        return print(f'Скорость автомобиля {self.name} - {self.speed} км/ч')
 
     def __str__(self):
         return f"Name: {self.name}\n" \
@@ -31,26 +30,32 @@ class Car:
 
 class TownCar(Car):
     def show_speed(self):
-        if self.speed > 60:
-            print(f'Скорость автомобиля {self.name} перевышена')
         super().show_speed()
+        if self.speed > 60:
+            print(f'Скорость перевышена!!!')
 
 
 class WorkCar(Car):
     def show_speed(self):
-        if self.speed > 40:
-            print(f'Скорость автомобиля {self.name} перевышена')
         super().show_speed()
+        if self.speed > 40:
+            print(f'Скорость перевышена!!!')
 
 
-car = Car(speed=40, color='green', name='bmw')
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police=True):
+        super().__init__(speed, color, name)
+        self.is_police = is_police
+
+
+car = Car(speed=40, color='green', name='BMW')
 
 town_car = TownCar(speed=65, color='green', name='Audi')
 
+police_car = PoliceCar(speed=55, color='black', name='Custom')
+
 print(car)
-
 print(town_car)
+print(police_car)
+
 town_car.show_speed()
-
-
-
