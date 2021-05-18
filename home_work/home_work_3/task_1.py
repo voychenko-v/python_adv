4c034c75936f1e97676ba538b63402d64424f2c2
voychenko-v <voychenkovitaliy@gmail.com>
2021-05-19 01:04:18 +0300

    git

---
+++ home_work/home_work_3/task_1.py
@@ -0,0 +1,26 @@
+def string_rename(check_string):
+    tmp_1 = ''
+    tmp_2 = ''
+    tmp_string = str(check_string).lower()
+    if tmp_string.count('one'):
+        tmp_1 += 'one'
+        tmp_2 += 'Один'
+    elif tmp_string.count('two'):
+        tmp_1 += 'two'
+        tmp_2 += 'Два'
+    elif tmp_string.count('three'):
+        tmp_1 += 'three'
+        tmp_2 += 'Три'
+    elif tmp_string.count('four'):
+        tmp_1 += 'four'
+        tmp_2 += 'Четыре'
+    return str(check_string).lower().replace(tmp_1, tmp_2)
+
+
+with open('old_list.txt', 'r') as f:
+    tmp_str = ''
+    for line in f:
+        tmp = string_rename(line)
+        tmp_str += tmp
+    with open('new_list.txt', 'a') as f_2:
+        print(tmp_str, file=f_2)
--- home_work/home_work_3/test_hw3.py
+++ home_work/home_work_3/test_hw3.py
@@ -1,2 +1,10 @@
-def test_hw3():
-    pass
+from .task_1 import string_rename
+import pytest
+
+
+@pytest.mark.parametrize('fun_on, fun_off', [('One', 'Один'), ('Two', 'Два'), ('Three', 'Три')])
+def test_hw3(fun_on, fun_off):
+    tmp_res = string_rename(fun_on)
+    assert tmp_res == fun_off
+
+
