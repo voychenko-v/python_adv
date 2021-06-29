65749086d87469ad9c6faf3121a8456a15564575
voychenko-v <voychenkovitaliy@gmail.com>
2021-06-30 01:39:32 +0300

    hw_5 v1

--- home_work/home_work_5/db.py
+++ home_work/home_work_5/db.py
@@ -1,25 +1,27 @@
 """"
-create table if not exists orders (
-	order_id SERIAL primary key,
-	created_dt date not null,
-	updated_dt date,
-	order_type text not null,
-	description text not null,
-	status text not null,
-	serial_no integer not null,
-	creator_id integer not null
-	);
+CREATE TABLE IF NOT EXISTS orders (
+    order_id SERIAL PRIMARY KEY,
+    created_dt DATE NOT NULL,
+    updated_dt DATE,
+    order_type TEXT NOT NULL,
+    description TEXT NOT NULL,
+    status TEXT NOT NULL,
+    serial_no INTEGER NOT NULL,
+    creator_id INTEGER NOT NULL
+    );
 
-create table if not exists employees (
-	employee_id SERIAL primary key,
-	fio text not null,
-	position text not null,
-	department_id integer not null
-	);
+CREATE TABLE IF NOT EXISTS departments (
+    department_id SERIAL PRIMARY KEY,
+    department_name TEXT NOT NULL
+    );
 
-create table if not exists departments (
-	department_id SERIAL primary key,
-	director_id integer not null,
-	department_name text not null
-	);
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id SERIAL PRIMARY KEY,
+    fio TEXT NOT NULL,
+    position TEXT NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES departments (department_id)
+    ON UPDATE SET NULL
+    ON DELETE CASCADE
+    );
 """
