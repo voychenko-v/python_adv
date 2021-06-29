cb9d03b5efa8c8d729fd8ccca02b98c7ae96f0ed
voychenko-v <voychenkovitaliy@gmail.com>
2021-06-30 01:30:44 +0300

    hw_3 v1

--- home_work/home_work_3/task_1.py
+++ home_work/home_work_3/task_1.py
@@ -1,27 +1,20 @@
 def string_rename(check_string):
-    tmp_1 = ''
-    tmp_2 = ''
-    tmp_string = str(check_string).lower()
-    if tmp_string.count('one'):
-        tmp_1 += 'one'
-        tmp_2 += 'Один'
-    elif tmp_string.count('two'):
-        tmp_1 += 'two'
-        tmp_2 += 'Два'
-    elif tmp_string.count('three'):
-        tmp_1 += 'three'
-        tmp_2 += 'Три'
-    elif tmp_string.count('four'):
-        tmp_1 += 'four'
-        tmp_2 += 'Четыре'
-    return str(check_string).lower().replace(tmp_1, tmp_2)
+    dict_num = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+    for key in dict_num:
+        if check_string.count(key):
+            return check_string.replace(key, dict_num[key])
 
 
-with open('old_list.txt', 'r') as f:
-    tmp_str = ''
-    for line in f:
-        tmp = string_rename(line)
-        tmp_str += tmp
+with open('old_list.txt', 'r', encoding="utf-8") as f:
     with open('new_list.txt', 'a', encoding="utf-8") as f_2:
-        print(tmp_str, file=f_2)
+        for line in f:
+            tmp = string_rename(line)
+            if tmp is None:
+                tmp = line
+            print(tmp, file=f_2, end='')
+
+
+
+
+
 
